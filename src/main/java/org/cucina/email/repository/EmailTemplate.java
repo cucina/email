7b13d355df52901402c8bd834b88f073c03dc230
vlevine <viktor.levine@gmail.,com>
2016-05-25 18:24:54 +0100

    using swagger

--- src/main/java/org/cucina/email/repository/EmailTemplate.java
+++ src/main/java/org/cucina/email/repository/EmailTemplate.java
@@ -8,109 +8,74 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import org.cucina.email.model.Template;
 
 /**
  * JAVADOC for Class Level
  *
  * @author vlevine
-  */
+ */
 @Document
-public class EmailTemplate {
-    @Id
-    private BigInteger id;
-    private Date lastModified;
-    @Version
-    private Integer version;
-    private String body;
-    private String name;
+public class EmailTemplate extends Template {
+	@Id
+	private BigInteger id;
 
-    /**
-     * JAVADOC Method Level Comments
-     *
-     * @param body JAVADOC.
-     */
-    public void setBody(String body) {
-        this.body = body;
-    }
+	private Date lastModified;
 
-    /**
-     * JAVADOC Method Level Comments
-     *
-     * @return JAVADOC.
-     */
-    public String getBody() {
-        return body;
-    }
+	@Version
+	private Integer version;
 
-    /**
-     *
-     *
-     * @param id .
-     */
-    public void setId(BigInteger id) {
-        this.id = id;
-    }
+	/**
+	 *
+	 *
+	 * @param id .
+	 */
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
 
-    /**
-     *
-     *
-     * @return .
-     */
-    public BigInteger getId() {
-        return id;
-    }
+	/**
+	 *
+	 *
+	 * @return .
+	 */
+	public BigInteger getId() {
+		return id;
+	}
 
-    /**
-     * JAVADOC Method Level Comments
-     *
-     * @param lastModified JAVADOC.
-     */
-    public void setLastModified(Date lastModified) {
-        this.lastModified = lastModified;
-    }
+	/**
+	 * JAVADOC Method Level Comments
+	 *
+	 * @param lastModified JAVADOC.
+	 */
+	public void setLastModified(Date lastModified) {
+		this.lastModified = lastModified;
+	}
 
-    /**
-     * JAVADOC Method Level Comments
-     *
-     * @return JAVADOC.
-     */
-    public Date getLastModified() {
-        return lastModified;
-    }
+	/**
+	 * JAVADOC Method Level Comments
+	 *
+	 * @return JAVADOC.
+	 */
+	public Date getLastModified() {
+		return lastModified;
+	}
 
-    /**
-     * JAVADOC Method Level Comments
-     *
-     * @param name JAVADOC.
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 *
+	 *
+	 * @param version .
+	 */
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
 
-    /**
-     * JAVADOC Method Level Comments
-     *
-     * @return JAVADOC.
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     *
-     *
-     * @param version .
-     */
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    /**
-     *
-     *
-     * @return .
-     */
-    public Integer getVersion() {
-        return version;
-    }
+	/**
+	 *
+	 *
+	 * @return .
+	 */
+	public Integer getVersion() {
+		return version;
+	}
 }
---
+++ src/main/java/org/cucina/email/service/TemplateApiImpl.java
@@ -0,0 +1,69 @@
+package org.cucina.email.service;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import org.cucina.email.api.TemplateApi;
+import org.cucina.email.model.Template;
+import org.cucina.email.repository.EmailTemplate;
+import org.cucina.email.repository.TemplateRepository;
+
+@Component
+public class TemplateApiImpl implements TemplateApi {
+	private static final Logger LOG = LoggerFactory.getLogger(TemplateApiImpl.class);
+
+	@Autowired
+	private TemplateRepository templateRepository;
+
+	@Override
+	public Callable<ResponseEntity<Void>> createTemplate(String name, MultipartFile file,
+			String locale) {
+		if (!file.isEmpty()) {
+			EmailTemplate et = new EmailTemplate();
+
+			try {
+				byte[] bytes = file.getBytes();
+				String body = new String(bytes, StandardCharsets.UTF_8);
+				et.setBody(body);
+			} catch (IOException e) {
+				LOG.error("Oops", e);
+				// to cause 500 in DefaultHandlerExceptionResolver
+				throw new RuntimeException(e);
+			}
+
+			et.setName(buildName(name, locale));
+			et.setLastModified(new Date());
+			templateRepository.save(et);
+
+			return () -> new ResponseEntity<Void>(HttpStatus.CREATED);
+		}
+
+		// Should cause 400 in DefaultHandlerExceptionResolver
+		throw new HttpMessageNotReadableException("No file provided");
+	}
+
+	@Override
+	public Callable<ResponseEntity<Template>> getTemplate(String name, String locale) {
+		Template et = templateRepository.findByName(buildName(name, locale));
+		// TODO handle not found
+		
+		
+		return () -> new ResponseEntity<>(et, HttpStatus.OK);
+	}
+
+	private String buildName(String name, String locale) {
+		return name + ((locale == null) ? "" : ("_" + locale.toString()))
+				+ MimeMessagePreparatorFactory.TEMPLATE_SUFFIX;
+	}
+}
